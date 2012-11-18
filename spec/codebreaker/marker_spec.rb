588a6269af91bba7b8f5dc272beca1b20c3c76ef
hidehigo <hidehigo@gmail.com>
2012-11-18 11:39:48 +0900

    modify spec but red

--- spec/codebreaker/marker_spec.rb
+++ spec/codebreaker/marker_spec.rb
@@ -8,25 +8,25 @@ module Codebreaker
     describe "#exact_match_count" do
       context "with no matches" do
         it "returns 0" do
-          marker = Marker.new('1234', '5555')
+          marker = Marker.new('123', '567')
           marker.exact_match_count.should == 0
         end
       end
-      context "with 1 number match" do
+      context "with 1 exact match" do
         it "returns 1" do
-          marker = Marker.new('1234', '1555')
+          marker = Marker.new('123', '156')
           marker.exact_match_count.should == 1
         end
       end
       context "with 1 number match" do
         it "returns 0" do
-          marker = Marker.new('1234', '2555')
+          marker = Marker.new('123', '256')
           marker.exact_match_count.should == 0
         end
       end
       context "with 1 exact match and 1 number match" do
         it "returns 1" do
-          marker = Marker.new('1234', '1525')
+          marker = Marker.new('123', '152')
           marker.exact_match_count.should == 1
         end
       end
@@ -35,25 +35,25 @@ module Codebreaker
     describe "#number_match_count" do
       context "with no matches" do
         it "returns 0" do
-          marker = Marker.new('1234', '5555')
+          marker = Marker.new('123', '567')
           marker.number_match_count.should == 0
         end
       end
       context "with 1 number match" do
         it "returns 1" do
-          marker = Marker.new('1234', '2555')
+          marker = Marker.new('123', '255')
           marker.number_match_count.should == 1
         end
       end
       context "with 1 exact match" do
         it "returns 0" do
-          marker = Marker.new('1234', '1555')
+          marker = Marker.new('123', '157')
           marker.number_match_count.should == 0
         end
       end
       context "with 1 exact match and 1 number match" do
         it "returns 1" do
-          marker = Marker.new('1234', '1525')
+          marker = Marker.new('123', '152')
           marker.exact_match_count.should == 1
         end
       end
@@ -62,7 +62,7 @@ module Codebreaker
     describe "#number_match_count" do
       context "with 1 exact match duplicated in guess" do
         it "returns 0" do
-          marker = Marker.new('1234', '1155')
+          marker = Marker.new('123', '115')
           marker.number_match_count.should == 0
         end
       end
